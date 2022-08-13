0558dba339ab4debd11c0e40ecdc91509e0a6efb
ensamuel <ensamuel11@gmail.com>
2022-08-13 15:38:02 +0100

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let marked = require('marked');
+
+export default class App extends React.Component{
+ constructor(props){
+  super(props);
+  this.state = {
+    markdown: '  <h1>We make history</h1><h2>We make change</h2><h4><a href="www.google.com">our site</a></h4><p>this is it</p><ul><li>credible</li><li>reliable</li></ul><blockquote>people with passion</blockquote><img src="images\antenna.jpg" alt="antenna" /><p><b>we are happy to serve you</b></p>'
+  }
+  this.handleChange = this.handleChange.bind(this)
+ } 
+
+  handleChange(markdown){
+    this.setState({markdown})
+  }
+  render(){
+
+    return(
+      <div>
+
+        <div className='header'>
+        Markdown Previewer
+        </div>
+
+        <div className="container">
+          <div className="markdownInputing">
+            <h4>Markdown input</h4>
+
+           <textarea name="markdown input" id="editor" style={{resize:'none',height: '300px', width:'300px', fontSize:'20px'}}
+           value={this.state.markdown} onChange={(e)=>{this.handleChange(e.target.value)}}
+           >
+           </textarea>
+          </div>
+
+          <div className="markdownPreviewer">
+          <h4>Preview</h4>
+          <div id='preview' style={{backgroundColor:'gray', height: '20rem', width:'20rem'}}
+          dangerouslySetInnerHTML={{
+            __html: marked.parse(this.state.markdown)
+          }}
+          >
+          </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
